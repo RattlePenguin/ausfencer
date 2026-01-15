83e7a22fac80939c92c04273d78576aade2d3ec2
RattlePenguin <viggo.setiawan@gmail.com>
2026-01-15 22:09:00 +0800

    Added a few draft messages for the user on cli startup.

---
+++ cli/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Welcome to Fencing Tournament Tracker!" << '\n';
+	
+	std::cout << "Type 1 to add a member." << '\n';
+	std::cout << "Type 2 to create a pool." << '\n';
+	std::cout << "Type 3 to create a standard tournament (pools + DEs)" << '\n';
+}
+
+
