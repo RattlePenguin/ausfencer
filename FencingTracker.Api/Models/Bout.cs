cd8e3e0d89a14f8b3423b4333107243f7d9a1be3
RattlePenguin <viggo.setiawan@gmail.com>
2026-01-21 19:22:07 +0800

    Updated Bout.cs to utilise public properties. Added the rest of the fencing data.

--- FencingTracker.Api/Models/Bout.cs
+++ FencingTracker.Api/Models/Bout.cs
@@ -1,18 +1,27 @@
 namespace FencingTracker.Api.Models;
 
-class Bout {
+public class Bout {
 	// ID related data
-	private Guid Id;
-	private string LeftFencerName;
-	private string RightFencerName;
+	public Guid Id { get; set; } = Guid.NewGuid();
+
+	public string LeftFencerName { get; set; } = string.Empty;
+	public string RightFencerName { get; set; } = string.Empty;
 
 	// Fencing related data
-	private int LeftScore;
-	private int RightScore;
-	
-	private int LeftYellowCards;
-	private int RightYellowCards;
-	private int LeftRedCards;
-	private int RightRedCards;
+	public int LeftScore { get; set; }
+	public int RightScore { get; set; }
 
+	public int TimeRemaining { get; set; } = 180;
+	public bool TimeRunning { get; set; }
+	
+	public int LeftYellowCards { get; set; }
+	public int RightYellowCards { get; set; }
+	public int LeftRedCards { get; set; }
+	public int RightRedCards { get; set; }
+
+	public Weapon Weapon { get; set; } = Weapon.Foil;
+	public BoutStatus Status { get; set; } = BoutStatus.NotStarted;
+
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	public DateTime? FinishedAt { get; set; }
 }
