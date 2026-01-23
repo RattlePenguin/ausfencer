9bea24c800bb1c06125bc4abf9aa7afc12fac867
RattlePenguin <viggo.setiawan@gmail.com>
2026-01-23 13:08:47 +0800

    Added Controller mapping.

--- FencingTracker.Api/Program.cs
+++ FencingTracker.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -14,6 +15,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 	app.MapScalarApiReference();
 }
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
