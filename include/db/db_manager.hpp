a7803aa8107af2824113bfbb90014f3d282b61e5
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-10 19:58:02 +1000

    TODO creating all database CRUD operations - add bout - get all bouts - get bout with id
    After this, create api routes to return database output, add parsing if
    required

--- include/db/db_manager.hpp
+++ include/db/db_manager.hpp
@@ -2,9 +2,14 @@
 #include <sqlite3.h>
 
 class DbManager {
-  DbManager(std::string dbName) {
+  DbManager(char *dbName) {
     sqlite3 *db;
-    sqlite3_open()
+    sqlite3_open(dbName, &db);
   }
   ~DbManager() {}
+
+  // adds bout to db
+  void addBout(Bout bout);
+
+  Bout getBout();
 };
