95de5046886515382b9cd38c283626d7f2381fec
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-11 04:33:28 +1000

    using sqlite_orm example to test

--- include/db/db_manager.hpp
+++ include/db/db_manager.hpp
@@ -3,14 +3,17 @@
 #include "../bout.hpp"
 
 class DbManager {
-  DbManager(char *dbName) {
-    sqlite3 *db;
-    sqlite3_open(dbName, &db);
-  }
+  DbManager(const std::string &dbName) { auto storage{createStorage(dbName)}; }
   ~DbManager() {}
 
-  // adds bout to db
-  void addBout(Bout bout);
-
-  Bout getBout();
+  inline auto createStorage(const std::string &filename) {
+    return sqlite_orm::make_storage(
+        dbName,
+        sqlite_orm::make_table("fencers",
+                               sqlite_orm::make_column(
+                                   "id", &Fencer::id, sqlite_orm::primary_key(),
+                                   sqlite_orm::auto_increment()),
+                               sqlite_orm::make_column("name", &Fencer::name),
+                               sqlite_orm::make_column("age", &Fencer::age)));
+  }
 };
