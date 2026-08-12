f3f034817a92037c999a059172acca6eb6ebfd41
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-13 01:24:31 +1000

    updated main to create server config then start server

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
-#include "sqlite3.h"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-  sqlite3 *db;
-  sqlite3_open("db.sqlite", &db);
-  sqlite3_close(db);
 
-  // start up the server so phones can send and receive requests
-  // and web pages are active
-  runServer();
-
-  // open our database for access
-
-  // tap start bout to pull up remote page
-  //
+  try {
+    ServerConfig
+  } catch (const std::exception &e) {
+    std::cout << e.what() << '\n';
+  }
 }
