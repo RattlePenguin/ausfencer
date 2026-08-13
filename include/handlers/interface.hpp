3777f43c3be39703596dbb787b2eea187480641a
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-13 20:28:39 +1000

    changed to snake case for consistency

--- include/handlers/interface.hpp
+++ include/handlers/interface.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
-#include "middleware/logger.hpp"
+#include "../middleware/logger.hpp"
 #include <crow/app.h>
 #include <crow/middlewares/cors.h>
 
 using App = crow::App<crow::CORSHandler, LoggerMiddleware>;
 
 class IHandler {
+public:
   virtual ~IHandler() = default;
-  virtual void registerRoutes(App &app);
+  virtual void register_routes(App &app) = 0;
+  virtual std::string get_name() const = 0;
 };
