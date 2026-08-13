23aad799b87087f0bbc8fdf2e22519adb9fb1faf
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-14 04:23:15 +1000

    added sqlite_orm create db storage helper func in db_manager.hpp, needed for the decltype for member variable in DbManager.
    This probably means we'll define the schema here.

--- include/db/db_manager.hpp
+++ include/db/db_manager.hpp
@@ -1,6 +1,29 @@
+#pragma once
+
+#include "models/Fencer.hpp"
 #include <mutex>
 #include <shared_mutex>
 #include <sqlite3.h>
+#include <sqlite_orm/sqlite_orm.h>
+#include <string>
+
+namespace sql = sqlite_orm;
+
+/**
+  Creates the sqlite file with db_name, then populates with the db schema
+  defined here. Normally bad practice in header files, but I need the decltype
+  for the Storage definition.
+*/
+inline auto create_db_storage(const std::string &db_name) {
+  return sql::make_storage(
+      db_name,
+      sql::make_table("fencers",
+                      sql::make_column("id", &Fencer::id,
+                                       sql::primary_key().autoincrement()),
+                      sql::make_column("first_name", &Fencer::first_name),
+                      sql::make_column("last_name", &Fencer::last_name),
+                      sql::make_column("birth_year", &Fencer::birth_year)));
+}
 
 #include "../models/Fencer.hpp"
 
