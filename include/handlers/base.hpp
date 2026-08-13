a4a80e9bea025090a598b93d3f576421033fa84b
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-13 20:26:14 +1000

    Added basehandler class header,
    Stores basePath. This is so that for handlers that inherit from
    BaseHandler, they prepend basePath to routes.
    
    Stores name so we know which handler it is. This will be updated for
    different handlers.
    
    Utility functions to handle errors correctly.

--- include/handlers/base.hpp
+++ include/handlers/base.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "interface.hpp"
+
+class BaseHandler : public IHandler {
+protected:
+  std::string basePath_;
+  std::string name_;
+
+  // Utility functions
+  static crow::response bad_request(const std::string &message);
+  static crow::response not_found(const std::string &message);
+  static crow::response internal(const std::string &message);
+
+public:
+  explicit BaseHandler(const std::string &basePath);
+  virtual ~BaseHandler() = default;
+  std::string get_name() const override;
+};
