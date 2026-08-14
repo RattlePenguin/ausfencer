f7a59f644a5697f7fc470195ae2daafa3bd8624a
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-15 06:15:05 +1000

    Implementing get_all function and added its route in registerRoutes.
    Using url_params to retrieve page and limit, as well as possible q param which acts as filter.
    Realising that we probably need to pass q, page and limit to fencerrepo,
    so that it only retrieves necessary data for efficiency.

--- src/handlers/fencers.cpp
+++ src/handlers/fencers.cpp
@@ -10,4 +10,41 @@ FencerHandler::FencerHandler(const std::string &basePath)
   this->name_ = "FencerHandler";
 };
 
-      };
+crow::response FencerHandler::get_all(const crow::request &req) {
+  auto q = req.url_params.get("q") ? req.url_params.get("q") : "";
+
+  // Indicates page of fencers
+  int page{1};
+  auto page_param =
+      req.url_params.get("page") ? req.url_params.get("page") : "1";
+
+  try {
+    page = std::max(page, std::stoi(page_param));
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  // Indicates limit of how many entries on one page
+  int limit{10};
+  auto limit_param =
+      req.url_params.get("limit") ? req.url_params.get("limit") : "10";
+
+  try {
+    limit = std::max(limit, std::stoi(limit_param));
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+  }
+
+  crow::json::wvalue res;
+  crow::json::wvalue::list fencers;
+}
+crow::response FencerHandler::get(int id);
+crow::response FencerHandler::create(const crow::request &req);
+crow::response FencerHandler::update(int id, const crow::request &req);
+crow::response FencerHandler::remove(int id);
+
+void FencerHandler::register_routes(App &app) {
+  app.route_dynamic(this->basePath_)
+      .methods(crow::HTTPMethod::GET)(
+          [this](const crow::request &req) { return this->get_all(req); });
+}
