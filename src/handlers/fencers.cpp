91e024c13ef31038cd59896e5a544ea5cec9b0cd
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-15 06:13:54 +1000

    Added handler name, about to implement CRUD operations

--- src/handlers/fencers.cpp
+++ src/handlers/fencers.cpp
@@ -1,7 +1,13 @@
 #include "handlers/fencers.hpp"
 #include "models/Fencer.hpp"
+#include <crow/common.h>
+#include <crow/http_request.h>
+#include <crow/json.h>
+#include <exception>
 
 FencerHandler::FencerHandler(const std::string &basePath)
     : BaseHandler(basePath) {
+  this->name_ = "FencerHandler";
+};
 
       };
