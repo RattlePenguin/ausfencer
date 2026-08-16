51a328312dbdf992e70deb3669f0e81697229e88
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-17 01:28:51 +1000

    added comments to fhandler header funcs

--- include/handlers/fencers.hpp
+++ include/handlers/fencers.hpp
@@ -7,10 +7,25 @@
 class FencerHandler : public BaseHandler {
   std::shared_ptr<FencerRepo> repo_;
 
+  // Returns all fencers, capped by pagination and limits
+  // req url params: q, page, limit
+  // q: search pattern on fencer first/last name
+  // page: number to offset by based on limit
+  // limit: max number of rows/entries returned at once
   crow::response get_all(const crow::request &req);
+
+  // Returns the fencer with the given id, else returns not found
   crow::response get(int id);
+
+  // Creates the fencer with the given id
+  // TODO Currently accepts duplicate fencers without distinguishing
+  // Returns the id of the fencer
   crow::response create(const crow::request &req);
+
+  // Updates the fencer with the given id
   crow::response update(int id, const crow::request &req);
+
+  // Removes the fencer with the given id
   crow::response remove(int id);
 
 public:
