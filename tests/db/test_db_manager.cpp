766770db5806eb87a4b4d89fa21f024dde496d0c
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-17 03:25:33 +1000

    added db mgr test: storage creation and dummy insert.

--- tests/db/test_db_manager.cpp
+++ tests/db/test_db_manager.cpp
@@ -1,10 +1,12 @@
 #include "db/db_manager.hpp"
 #include <gtest/gtest.h>
+
 class DbManagerTest : public ::testing::Test {
 protected:
   void SetUp() override {}
   void TearDown() override {}
 };
+
 TEST_F(DbManagerTest, InitializeInMemoryDatabase) {
   EXPECT_NO_THROW({
     DbManager db_mgr(":memory:");
@@ -12,3 +14,20 @@ TEST_F(DbManagerTest, InitializeInMemoryDatabase) {
     EXPECT_TRUE(lock.owns_lock());
   });
 }
+
+TEST_F(DbManagerTest, StorageTableCreationAndSyncSchema) {
+  DbManager db_mgr(":memory:");
+  auto &storage = db_mgr.get_storage();
+  
+  // Verify storage is functional by inserting and retrieving a fencer
+  Fencer fencer{0, "Test", "User", 2000};
+  int id = storage.insert(fencer);
+  EXPECT_GT(id, 0);
+
+  auto retrieved = storage.get_pointer<Fencer>(id);
+  ASSERT_NE(retrieved, nullptr);
+  EXPECT_EQ(retrieved->first_name, "Test");
+  EXPECT_EQ(retrieved->last_name, "User");
+  EXPECT_EQ(retrieved->birth_year, 2000);
+}
+
