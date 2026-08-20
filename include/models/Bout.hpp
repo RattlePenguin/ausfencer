d037c09fd51439af042fe63c51cf3272716254e6
RattlePenguin <viggo.setiawan@gmail.com>
2026-08-20 17:40:19 +1000

    modified bout model to store fencer id instead, foreign key in db

--- include/models/Bout.hpp
+++ include/models/Bout.hpp
@@ -1,20 +1,19 @@
 #pragma once
 
-#include "Fencer.hpp"
-
 enum Weapon { Foil, Epee, Sabre };
 
 struct Bout {
   int id;
-  Fencer left_fencer;
-  Fencer right_fencer;
+  int left_fencer_id;
+  int right_fencer_id;
 
   Weapon weapon;
-  int time;
+  int time; // seconds
 
-  int left_score;
+  int left_score; // should be capped at 99
   int right_score;
 
+  // usually more than 1 yellow is not allowed
   int left_yellow;
   int right_yellow;
 
